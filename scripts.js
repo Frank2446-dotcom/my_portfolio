b45df2d9e07a02c880b47f3c44210234d377e6cb
Frank2446-dotcom <150971995+Frank2446-dotcom@users.noreply.github.com>
2024-07-01 03:03:55 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -298,3 +298,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
 //JavaScript: Ensures the #bio section is shown by default when the page loads and when the logo is clicked.
 
+//PHONE NUMBER JS
+
+const phoneInputField = document.querySelector("#phone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+    utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
