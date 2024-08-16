4b631db8e16962c8827cddd08a814e443b6d8477
Frank2446-dotcom <150971995+Frank2446-dotcom@users.noreply.github.com>
2024-08-16 12:49:36 +0300

    Add files via upload

---
+++ My Portfolio/projects/archive/natural-disaster.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+# Load the dataset
+file_path = r'C:\Users\user\OneDrive\PLP Academy\Web Develpment\My Portfolio\projects\archive\natural-disasters.csv'
+data = pd.read_csv(file_path)
